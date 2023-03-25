3f19b74c24d143e95addc5b7effe8844c6737641
Andrew William Watson <aww@awwsmm.com>
2023-03-25 09:59:40 -0400

    checkpoint just above https://arzg.github.io/lang/10/\#writing-a-parser-that-does-things

---
+++ crates/lang/src/parser.rs
@@ -0,0 +1,66 @@
+use crate::lexer::SyntaxKind;
+use crate::syntax::{LangLanguage, SyntaxNode};
+use logos::Logos;
+use rowan::{GreenNode, GreenNodeBuilder, Language};
+
+pub struct Parser<'a> {
+    lexer: logos::Lexer<'a, SyntaxKind>,
+    builder: GreenNodeBuilder<'static>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            lexer: SyntaxKind::lexer(input),
+            builder: GreenNodeBuilder::new(),
+        }
+    }
+
+    pub fn parse(mut self) -> Parse {
+        self.builder.start_node(LangLanguage::kind_to_raw(SyntaxKind::Root));
+        self.builder.finish_node();
+
+        Parse {
+            green_node: self.builder.finish(),
+        }
+    }
+
+    fn start_node(&mut self, kind: SyntaxKind) {
+        self.builder.start_node(LangLanguage::kind_to_raw(kind))
+    }
+
+    fn finish_node(&mut self) {
+        self.builder.finish_node();
+    }
+}
+
+pub struct Parse {
+    green_node: GreenNode,
+}
+
+impl Parse {
+    pub fn debug_tree(&self) -> String {
+        let syntax_node  = SyntaxNode::new_root(self.green_node.clone());
+        let formatted = format!("{:#?}", syntax_node);
+
+        // We cut off the last byte because formatting the SyntaxNode adds on a newline at the end.
+        formatted[0..formatted.len() - 1].to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::syntax::SyntaxNode;
+    use expect_test::{expect, Expect};
+
+    fn check(input: &str, expected_tree: Expect) {
+        let parse = Parser::new(input).parse();
+        expected_tree.assert_eq(&parse.debug_tree());
+    }
+
+    #[test]
+    fn parse_nothing() {
+        check("", expect![[r#"Root@0..0"#]]);
+    }
+}
